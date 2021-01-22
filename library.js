78f55655a194404d5272dade7021ef890c61af77
Emma Hack <ehack78@gmail.com>
2021-01-22 15:59:27 +0000

    initial commit book and library class refactored

---
+++ library.js
@@ -0,0 +1,25 @@
+class Library{
+  constructor(books = []) {
+  this.books = books;
+}
+};
+
+Library.prototype.bookCount = function () {
+  return this.books.length;
+};
+
+Library.prototype.addBook = function (newBook) {
+  this.books.push(newBook);
+};
+
+Library.prototype.addBooks = function (newBooks) {
+  newBooks.forEach(book => this.books.push(book));
+};
+
+Library.prototype.printInventory = function () {
+  this.books.forEach((book) => {
+    console.log(`${book.title} by ${book.author}`);
+  });
+};
+
+module.exports = Library;
